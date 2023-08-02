b393b6c03158967deda446374e5406bb67a2f300
arpruss <arpruss@gmail.com>
2023-08-01 22:06:37 -0500

    more code cleanup

--- build.py
+++ build.py
@@ -10,19 +10,15 @@ name = "BigAndSimple"
 
 Digits = namedtuple("Digits", ("color", "size", "file", "index", "blankZero"), defaults=[None,None,None,None,False])
 
-batteryHeight = 2
-batteryWidth = 128
-batteryBackColor = (64,64,64,255)
-batteryFullColor = (0,255,0,255)
-batteryEmptyColor = (255,0,0,255)
+Battery = namedtuple("Battery", ("size", "backColor", "fullColor", "emptyColor", "index"))
 
 bigDigit = Digits(color = (255,255,255,255), size = (60,86), file = "heavy%01d.png", index=0)
 bigDigitHourTensBlankZero = Digits(color = (255,255,255,255), size = (60,86), file = "heavy%01d.png", index=10, blankZero = True)
 bigDigitHourTensZeroZero = Digits(color = (255,255,255,255), size = (60,86), file = "heavy%01d.png", index=10, blankZero = False, )
 heartDigit = Digits(color = (255,153,85,255), size = (34,46), file = "bold%01d.png", index=20)
 stepsDigit = Digits(color = (255,255,0,255), size = (25,46), file = "bold%01d.png", index=30)
+battery = Battery(size=(128,2), backColor=(64,64,64,255), fullColor = (0,255,0,255), emptyColor = (255,0,0,255), index = 40)
 previewSize = (104,328)
-batteryIndex = 40
 previewIndex = 50
 buildDirectory = "build"
 
@@ -52,6 +48,19 @@ def generateDigits(digits):
     print("Generating %04d" % digits.index)
     for i in range(10):
         generateDigit(digits.file % i,digits.color,digits.size, "%s/%04d.png" % (buildDirectory, digits.index+i),blank=(digits.blankZero and i == 0))
+        
+def generateBattery(b):
+    print("Generating %04d" % b.index)
+    for i in range(10):
+        x = i / 9.
+        img = Image.new('RGBA', b.size, b.backColor)
+        draw = ImageDraw.Draw(img)
+        w = int(x * b.size[0])
+        c = []
+        for j in range(4):
+            c.append(int(b.fullColor[j]*x + b.emptyColor[j]*(1-x)))
+        draw.rectangle([(b.size[0]//2-w//2,0),(b.size[0]//2+w//2),b.size[1]],fill=tuple(c))
+        img.save("%s/%04d.png" % (buildDirectory, i+b.index), 'PNG')
     
 try:
     os.mkdir(buildDirectory)
@@ -65,18 +74,6 @@ for zero in (False,True):
         with open(json, "w") as g:
             g.write(f.read())
 
-    # generate battery
-    print("Generating %04d" % batteryIndex)
-    for i in range(10):
-        x = i / 9.
-        img = Image.new('RGBA', (batteryWidth,batteryHeight), batteryBackColor)
-        draw = ImageDraw.Draw(img)
-        w = int(x * batteryWidth)
-        c = []
-        for j in range(4):
-            c.append(int(batteryFullColor[j]*x + batteryEmptyColor[j]*(1-x)))
-        draw.rectangle([(batteryWidth//2-w//2,0),(batteryWidth//2+w//2),batteryHeight],fill=tuple(c))
-        img.save("%s/%04d.png" % (buildDirectory, i+batteryIndex), 'PNG')
         
     generateDigits(bigDigit)
     if zero:
@@ -85,6 +82,7 @@ for zero in (False,True):
         generateDigits(bigDigitHourTensBlankZero)
     generateDigits(heartDigit)
     generateDigits(stepsDigit)
+    generateBattery(battery)
     
     resize(json[:-5]+"_packed_preview.png", "%s/%04d.png" % (buildDirectory, previewIndex), previewSize)
     
