3ef825b3ee93d030c627aeca5028ea30000bbc12
arpruss <arpruss@gmail.com>
2023-08-01 22:27:26 -0500

    foo

--- build.py
+++ build.py
@@ -9,15 +9,17 @@ import os
 name = "BigAndSimple"
 
 Digits = namedtuple("Digits", ("color", "size", "file", "index", "blankZero"), defaults=[None,None,None,None,False])
-
 Battery = namedtuple("Battery", ("size", "backColor", "fullColor", "emptyColor", "index"))
+DateSlash = namedtuple("Slash", ("size", "file", "color", "index"))
 
 bigDigit = Digits(color = (255,255,255,255), size = (60,86), file = "heavy%01d.png", index=0)
 bigDigitHourTensBlankZero = Digits(color = (255,255,255,255), size = (60,86), file = "heavy%01d.png", index=10, blankZero = True)
 bigDigitHourTensZeroZero = Digits(color = (255,255,255,255), size = (60,86), file = "heavy%01d.png", index=10, blankZero = False, )
 heartDigit = Digits(color = (255,153,85,255), size = (34,46), file = "bold%01d.png", index=20)
 stepsDigit = Digits(color = (255,255,0,255), size = (25,46), file = "bold%01d.png", index=30)
+dateDigit = stepsDigit
 battery = Battery(size=(128,2), backColor=(64,64,64,255), fullColor = (0,255,0,255), emptyColor = (255,0,0,255), index = 40)
+dateSlash = None # DateSlash(file = "boldslash.png", size = dateDigit.size, color = dateDigit.color, index = 51)
 previewSize = (104,328)
 previewIndex = 50
 buildDirectory = "build"
@@ -31,7 +33,7 @@ def resize(inFile,outFile,size):
         img = Image.new('RGB', size)
     img.save(outFile,'PNG')
 
-def generateDigit(inFile,color,size,outFile,blank=False):
+def generateCharacter(inFile,color,size,index,blank=False):
     img = Image.open(inFile).convert('RGBA')
     img = img.resize(size,resample=Image.LANCZOS)
     for x in range(img.size[0]):
@@ -42,12 +44,13 @@ def generateDigit(inFile,color,size,outFile,blank=False):
                 pixel = img.getpixel((x,y))
                 out = tuple(color[i] if i < 3 else pixel[3] for i in range(4))
             img.putpixel((x,y),out)
+    outFile = "%s/%04d.png" % (buildDirectory, index)
     img.save(outFile, 'PNG')
 
 def generateDigits(digits):
     print("Generating %04d" % digits.index)
     for i in range(10):
-        generateDigit(digits.file % i,digits.color,digits.size, "%s/%04d.png" % (buildDirectory, digits.index+i),blank=(digits.blankZero and i == 0))
+        generateCharacter(digits.file % i,digits.color,digits.size, digits.index, blank=(digits.blankZero and i == 0))
         
 def generateBattery(b):
     print("Generating %04d" % b.index)
@@ -83,6 +86,8 @@ for zero in (False,True):
     generateDigits(heartDigit)
     generateDigits(stepsDigit)
     generateBattery(battery)
+    if dateSlash:
+        generateCharacter(dateSlash.file,dateSlash.color,dateSlash.size,dateSlash.index)
     
     resize(json[:-5]+"_packed_preview.png", "%s/%04d.png" % (buildDirectory, previewIndex), previewSize)
     
